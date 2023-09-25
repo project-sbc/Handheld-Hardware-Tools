8719abc3f70f0073dcc98ec963e4dfba18bb08cf
projectsbc <Project.SBC@outlook.com>
2023-09-25 08:58:43 -0400

    save

--- Classes/InverseBooleanConverter.cs
+++ Classes/InverseBooleanConverter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Markup;
 
@@ -43,5 +44,5 @@ namespace Everything_Handhelds_Tool.Classes.Converters
         #endregion
     }
 
-
+   
 }
