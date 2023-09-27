1bba50f9e5337e64353d0ecc298f2c2e6a65e831
projectsbc <Project.SBC@outlook.com>
2023-09-27 16:22:47 -0400

    save

---
+++ Classes/Models/Action.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everything_Handhelds_Tool.Classes.Models
+{
+    public class Action
+    {
+        string actionName;
+        string 
+
+
+    }
+}
