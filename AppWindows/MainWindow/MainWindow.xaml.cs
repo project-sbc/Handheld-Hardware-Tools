74aa81a6a54a1f8c61ea7e9c2c265c5ab122c4d4
ProjectSBC <project.sbc@outlook.com>
2024-03-19 06:29:42 -0400

    save

---
+++ AppWindows/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Everything_Handhelds_Tool.Classes.Controller_Object_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Everything_Handhelds_Tool.AppWindows.MainWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ControllerWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
